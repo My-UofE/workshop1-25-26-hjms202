774de95f8fcb780131459ddb8f06450ef96c878d
hjms202 <hjms202@exeter.ac.uk>
2026-01-16 13:11:31 +0000

    completed function for game 2

--- game_functions.py
+++ game_functions.py
@@ -7,7 +7,13 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
-    pass
+    if user_input == "h":
+        if next_val > current_val:
+            return True
+        return False
+    if current_val > next_val:
+        return True
+    return False
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
